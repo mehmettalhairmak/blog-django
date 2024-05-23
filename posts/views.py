cd876693af9f476920e4db0c5542ab925b85cb4f
Mehmet Talha Irmak <mehmetirmaakk@gmail.com>
2024-05-23 22:26:10 +0300

    index.html view codes

--- posts/views.py
+++ posts/views.py
@@ -3,4 +3,10 @@ from django.shortcuts import render
 
 # Create your views here.
 def index(request):
-    return render(request, 'index.html')
+    posts = Post.objects.all()
+    return render(request, 'index.html', {'posts': posts})
+
+
+def post(request, pk):
+    posts = Post.objects.get(id=pk)
+    return render(request, 'post.html', {'posts': posts})
